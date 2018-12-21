4706c11cfa91245fd9566a435175fc9d4e32cd83
github username <github email address>
2018-12-21 11:05:59 -0500

    Did not Crush It

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,41 @@
-class Article
 
+
+### Article
+# An article is initialized with an author as an Author object and a magazine as a Magazine object. An article **cannot** change its author or magazine after it is has been initialized.
+#
+class Article
+    attr_accessor :author, :name, :magazine
+
+    @@all = []
+
+    def initialize(author, name, magazine)
+        @author = author
+        @name = name
+        @magazine = magazine
+        @@all << self
+    end
+
+
+    # + `Article.all`
+    #   + Returns an array of all Article instances
+    def self.all
+        @@all
+    end
+
+    # + `Article#author`
+    #   + Returns the author for that given article
+    def author
+        @@all.select do |article|
+            self
+        end
+    end
+
+    # + `Article#magazine`
+    #   + Returns the magazine for that given article
+    def magazine
+        @@all.select do |article|
+            self
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -5,9 +5,28 @@ def reload
 end
 
 
-###  WRITE YOUR TEST CODE HERE ###
+ryan = Author.new("Ryan")
+arthur = Author.new("Arther")
+bryan = Author.new("Bryan")
+sarah = Author.new("Sarah")
+cat = Author.new("Cat")
+sonny = Author.new("Sonny")
 
+bimmerworld = Magazine.new("Bimmerworld", "cars")
+people = Magazine.new("People", "entertainment")
+vwtrends = Magazine.new("VW Trends", "cars")
+architecture = Magazine.new("Architecture", "design")
+us = Magazine.new("US", "entertainment")
+economist = Magazine.new("Economist", "finance")
 
+# attr_accessor :author, :name, :magazine
+m3vsm5 = Article.new(ryan, "M3vsM5", bimmerworld)
+busvsbug = Article.new(ryan, "Bus vs. Bug", vwtrends)
+holywoodinside = Article.new(arthur, "Inside Track on pop stars.", people)
+busesrock = Article.new(bryan, "Busses Rock!", vwtrends)
+buildingdesign = Article.new(sarah, "Building Design", architecture)
+cults = Article.new(cat, "Holywood cults", us)
+trends = Article.new(sonny, "Current Trends", economist)
 
 
 
