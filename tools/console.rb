70ccc47c1e360efdfe9cfcfa9f1c4cffab7e55b5
Chanda Grayson <chandagrayson@gmail.com>
2018-12-21 11:05:39 -0500

    crushed it

--- tools/console.rb
+++ tools/console.rb
@@ -7,8 +7,19 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Blair Ryan")
+author2 = Author.new("Brian Sarah")
+author3 = Author.new("Sunny Arthur")
 
 
+article1 = Article.new("What is Love", "Blair Ryan", "Flatiron")
+article2 = Article.new("Baby Don't Hurt Me", "Brian Sarah", "Nature Sounds")
+article3 = Article.new("No More", "Sunny Arthur", "Dogs")
+
+
+magazine1 = Magazine.new("Flatiron", "Tech")
+magazine2 = Magazine.new("Nature Sounds", "Nature")
+magazine3 = Magazine.new("Dogs", "Pets")
 
 
 
